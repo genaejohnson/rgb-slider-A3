0210ff41956380278c507fd693ad6337cecbc998
Genae Johnson <gsjohnson@wlapcs.org>
2018-01-22 18:56:40 +0000

    added sliders and background changing

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+var ruckerSlider, genaeSlider, bakerSlider;
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    ruckerSlider = createSlider(0,255,0);
+    ruckerSlider.position(20, 20);
+    
+    genaeSlider = createSlider(0, 255, 0);
+    genaeSlider.position(20, 50);
+    
+    bakerSlider = createSlider(0,255,0);
+    bakerSlider.position(20, 80);
 }
 
 function draw(){
+    redRuby = ruckerSlider.value();
+    greenSavage = genaeSlider.value();
+    blueKiller = bakerSlider.value();
     
+    background(redRuby,greenSavage,blueKiller);
 }
